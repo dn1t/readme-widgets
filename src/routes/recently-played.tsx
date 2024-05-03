f8120564476d5bf11a8cf63292b9cd6a1743facd
thoratica <me@tica.fun>
2024-05-04 01:24:54 +0900

    feat: textColor based on artwork

--- src/routes/recently-played.tsx
+++ src/routes/recently-played.tsx
@@ -6,6 +6,7 @@ interface Track {
 	artist: string;
 	artwork: string;
 	bgColor: string;
+	textColor: string;
 	width: number;
 	height: number;
 }
@@ -42,6 +43,8 @@ async function getRecentlyPlayed(
 		height = Math.round(width / aspectRatio);
 	}
 
+	console.log(attributes);
+
 	return {
 		success: true,
 		data: {
@@ -51,6 +54,7 @@ async function getRecentlyPlayed(
 				.replace("{w}", width)
 				.replace("{h}", height),
 			bgColor: `#${attributes.artwork.bgColor}`,
+			textColor: `#${attributes.artwork.textColor1}`,
 			width,
 			height,
 		},
@@ -80,7 +84,7 @@ export async function RecentlyPlayed(params: URLSearchParams) {
 				position: "relative",
 				width: 640,
 				height: 300,
-				backgroundColor: "#324f68",
+				backgroundColor: track.bgColor,
 				borderWidth: 3,
 				borderStyle: "solid",
 				borderColor: "rgba(255, 255, 255, 0.15)",
@@ -132,7 +136,7 @@ export async function RecentlyPlayed(params: URLSearchParams) {
 							fill-rule="evenodd"
 							clip-rule="evenodd"
 							d="M31.835 31.4516C31.9918 30.7069 31.9984 30.0307 31.9984 29.286H32V1.55221C32 0.546149 31.4692 -0.074476 30.5253 0.00718514C30.3832 0.0202509 29.1208 0.244002 28.9689 0.275034L11.4943 3.80116L11.4878 3.8028C11.0321 3.89916 10.6761 4.06084 10.3984 4.29276C10.0653 4.57204 9.88074 4.96728 9.81052 5.42785C9.79582 5.52585 9.77132 5.7251 9.77132 6.01908V27.8912C9.77132 28.4008 9.73049 28.8989 9.38427 29.3219C9.03804 29.7449 8.60853 29.8723 8.10879 29.9736L6.96722 30.2038C5.52352 30.4946 4.58447 30.6922 3.7336 31.0221C2.9203 31.3389 2.31114 31.7407 1.8261 32.2486C0.864176 33.2563 0.473855 34.6234 0.607774 35.9038C0.723728 36.9948 1.21367 38.0401 2.058 38.8142C2.62797 39.3352 3.34002 39.7337 4.17945 39.9019C5.05155 40.0767 5.97754 40.0146 7.33305 39.7419C8.05489 39.5965 8.72938 39.3695 9.37447 38.989C10.0114 38.6133 10.5569 38.1119 10.9831 37.5011C11.4094 36.8886 11.687 36.2076 11.8389 35.4841C11.994 34.7393 12.0332 34.0632 12.0332 33.3184V14.3485C12.0332 13.331 12.3207 13.0632 13.1405 12.8656C13.1405 12.8656 27.6673 9.93555 28.3434 9.80326C29.2906 9.62197 29.7348 9.89145 29.7348 10.8828V23.8326C29.7348 24.3455 29.7283 24.8632 29.3804 25.2895C29.0342 25.7125 28.6047 25.8399 28.105 25.9411L26.9634 26.1714C25.5197 26.4621 24.5806 26.6598 23.7298 26.9897C22.9165 27.3065 22.3073 27.7083 21.8223 28.2162C20.862 29.2239 20.4357 30.5909 20.5696 31.8714C20.684 32.9624 21.2098 34.0076 22.0542 34.7818C22.6241 35.3044 23.3362 35.6915 24.1756 35.8597C25.0477 36.0345 25.9737 35.9724 27.3292 35.6996C28.0511 35.5543 28.7255 35.3371 29.3706 34.9565C30.0076 34.5809 30.553 34.0795 30.9793 33.4687C31.4055 32.8562 31.6832 32.1751 31.835 31.4516Z"
-							fill="white"
+							fill={track.textColor}
 						/>
 					</svg>
 				</div>
@@ -153,7 +157,7 @@ export async function RecentlyPlayed(params: URLSearchParams) {
 							fontWeight: 600,
 							fontSize: 22,
 							lineHeight: "26px",
-							color: "rgba(255, 255, 255, 0.8)",
+							color: `${track.textColor}cc`,
 							width: 308,
 						}}
 					>
@@ -165,7 +169,7 @@ export async function RecentlyPlayed(params: URLSearchParams) {
 							fontWeight: 700,
 							fontSize: 36,
 							lineHeight: "43px",
-							color: "#fff",
+							color: track.textColor,
 							width: 308,
 						}}
 					>
@@ -177,7 +181,7 @@ export async function RecentlyPlayed(params: URLSearchParams) {
 							fontWeight: 500,
 							fontSize: 32,
 							lineHeight: "38px",
-							color: "rgba(255, 255, 255, 0.8)",
+							color: `${track.textColor}cc`,
 						}}
 					>
 						{track.artist}
